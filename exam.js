a39972f973f3724194ab23c73d7b3709c44af12e
unknown <Blessinggianna7@gmail.com>
2023-02-13 16:25:26 +0200

    First commit

---
+++ exam.js
@@ -0,0 +1,67 @@
+
+const express = require('express');
+ const app = express();
+ const bodyParser= require('body-parser');
+ app.use(bodyParser.urlencoded({ extended: false })); 
+
+ app.get('/', (req,res)=>{
+    res.sendFile('./exam.hmtl');
+ })
+  //multiplication
+ app.post('/multiplication' , (req,res)=>{
+ let num1= parseInt(req.params.num1);
+ let num2= parseInt(req.params.num2);
+ let product= num1*num2;
+ res.send(product);
+})
+
+   
+
+
+
+ //subtract
+app.post('/subtract' , (req,res)=>{
+    let num1= parseInt(req.params.num1);
+    let num2= parseInt(req.params.num2);
+    let diff=num1-num2;
+
+res.send(diff);
+})
+  
+
+
+
+
+
+
+//addition
+app.post('/add' , (req,res)=>{
+let num1= parseInt(req.params.num1);
+ let num2=parseInt(req.params.num2);
+ let sum= num1+num2;
+ res.send(sum);
+})
+
+
+
+
+
+//division
+ app.post('/divide' , (req,res)=>{
+    let num1= parseInt(req.params.num1);
+ let num2=parseInt(req.params.num2);
+ let division= (num1)/(num2);
+ res.send(division);
+})        
+
+
+
+
+
+
+
+
+
+
+
+  app.listen(3300, () => { console.log(`Calculator app listening at port 3300`); }); 
